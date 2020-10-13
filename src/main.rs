0b9786523e65874fbdc6fcba897647e4e0236cba
Joseph Levine <joseph.levine@senate.mn>
2020-10-13 15:30:41 -0500

    super basic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-use std::io::stdin;
-use std::process::Command;
+use std::io::{stdin, stdout, Write, Result, Error};
+use std::process::{Command, Child};
 
-fn main() {
-    print!(">");
+fn read() -> String {
     let mut buf = String::new();
     stdin().read_line(&mut buf).unwrap();
-    let program = buf.trim()
-    Command::new(program).spawn().unwrap();
+    String::from(buf.trim())
+}
+
+fn tokenize() {}
+
+fn parse() {}
+
+fn expand() {}
+
+fn redirect() {}
+
+fn execute(program: String) -> Result<Child> {
+    Command::new(program).spawn()
+}
+
+fn main() {
+    loop {
+        print!(">");
+        stdout().flush();
+        let program = read();
+        match execute(program) {
+            Ok(running) => { running.wait_with_output(); }
+            Err(error) => { eprintln!("System error: {}", error) }
+        }
+    }
 }
