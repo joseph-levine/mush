f91df596904cde3045f1423f00c331ef4188a0ff
Joseph Levine <joseph.levine@senate.mn>
2020-10-16 11:38:21 -0500

    current working directory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::{stdin, stdout, Write, Result, Error};
-use std::process::{Command, Child};
+use std::env::current_dir;
+use std::io::{stderr, stdin, stdout, Result, Write};
+use std::process::{Child, Command};
 
 fn read() -> String {
     let mut buf = String::new();
@@ -19,14 +20,28 @@ fn execute(program: String) -> Result<Child> {
     Command::new(program).spawn()
 }
 
+fn cwd() -> String {
+    if let Ok(os_cwd) = current_dir() {
+        if let Some(os_cwd) = os_cwd.to_str() {
+            return String::from(os_cwd);
+        }
+    }
+    String::from("mush err 0")
+}
+
 fn main() {
     loop {
-        print!(">");
-        stdout().flush();
+        print!("{}>", cwd());
+        let _ = stdout().flush();
         let program = read();
         match execute(program) {
-            Ok(running) => { running.wait_with_output(); }
-            Err(error) => { eprintln!("System error: {}", error) }
+            Ok(running) => {
+                let _ = running.wait_with_output();
+            }
+            Err(error) => {
+                eprintln!("System error: {}", error);
+                let _ = stderr().flush();
+            }
         }
     }
 }
